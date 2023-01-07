94b587b506bccf2864d223b6936b48621306155e
Vitico99 <nenevictor99@gmail.com>
2023-01-07 16:38:57 -0500

    Another test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ with open('confirmed_us.csv', 'r') as f:
     df = pd.read_csv(f)
 
 a = [0] * len(df['10-10-2020'])
-df['abc'] = a
+df['new column'] = a
 
 with open('confirmed_us.csv', 'w') as f:
     df.to_csv(f, index=False)
