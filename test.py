f4139e1efa0e672f889fbdb9e5eac623ce9490bc
Vitico99 <nenevictor99@gmail.com>
2023-01-07 16:28:29 -0500

    Test

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+with open('confirmed_us.csv', 'r') as f:
+    df = pd.read_csv(f)
+
+a = [0] * len(df['10-10-2020'])
+df['abc'] = a
+
+with open('confirmed_us.csv', 'w') as f:
+    df.to_csv(f, index=False)
